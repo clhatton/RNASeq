1666f5e396486e1afc42d228aa889a2e8e087455
Courtney Hatton <101742079+clhatton@users.noreply.github.com>
2024-01-27 14:23:22 -0500

    Add files via upload

---
+++ run-pipe-PE-STAR-hg38.sh
@@ -0,0 +1,93 @@
+#! /bin/bash
+set -e
+
+source ~/miniconda3/etc/profile.d/conda.sh
+#conda env create -n rnaseq --file rnaseq.yml
+conda activate rnaseq
+
+sample=$1
+#sample=test
+R1="$sample"_R1_001.fastq.gz
+R2="$sample"_R2_001.fastq.gz
+
+baseDir=/pi/zhiping.weng-umw/data/hattonc/PRODMAT_Corvera/RNASeq/PRODMAT/STAR/
+#baseDir=/home/courtney.hatton-umw/practice_PRODMAT/ 
+fastqDir=/pi/silvia.corvera-umw/project/prodmat/fastq/
+#fastqDir=/home/courtney.hatton-umw/test_fastq/
+referenceDir=/home/courtney.hatton-umw/data/genome/hg38/RSEM/STAR/
+fastqc_rawDir=$baseDir/FastQC_Raw
+fastqc_trimmedDir=$baseDir/FastQC_Trimmed
+bamDir=$baseDir/BAMs
+genesDir=$baseDir/Genes
+isoformsDir=$baseDir/Isoforms
+unpairedDir=$baseDir/Unpaired_Trimmed
+trimlogDir=$baseDir/Trimming_Logs
+trimsumDir=$baseDir/Trimming_Summary
+
+tempDir=/tmp/hattonc/$sample
+
+
+rm -rf $tempDir; mkdir -p $tempDir; cd $tempDir
+cp $fastqDir$R1 ./
+cp $fastqDir$R2 ./
+
+fastqc $R1
+fastqc $R2
+
+trimmomatic PE -threads 16 -trimlog $sample.trim.log -summary $sample.trim.summary \
+$R1 $R2 $sample.R1.P.fastq.gz $sample.R1.U.fastq.gz $sample.R2.P.fastq.gz $sample.R2.U.fastq.gz \
+ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+
+fastqc $sample.R1.P.fastq.gz 
+fastqc $sample.R2.P.fastq.gz 
+
+
+cp -r $referenceDir $tempDir
+
+gunzip $sample.R1.P.fastq.gz 
+gunzip $sample.R2.P.fastq.gz 
+
+rsem-calculate-expression \
+--paired-end \
+--star \
+--star-output-genome-bam \
+-p 16 \
+$sample.R1.P.fastq \
+$sample.R2.P.fastq \
+$tempDir/STAR/RSEM \
+$sample
+
+samtools sort -@ 16 $sample.STAR.genome.bam -o $sample.STAR.genome.sorted.bam
+samtools sort -@ 16 $sample.transcript.bam -o $sample.transcript.sorted.bam
+
+samtools index -@ 16 $sample.STAR.genome.sorted.bam
+samtools index -@ 16 $sample.transcript.sorted.bam
+
+cp $sample.genes.results $genesDir
+cp $sample.isoforms.results $isoformsDir
+cp $sample.STAR.genome.sorted.bam $bamDir
+cp $sample.transcript.sorted.bam $bamDir
+cp $sample.STAR.genome.sorted.bam.bai $bamDir
+cp $sample.transcript.sorted.bam.bai $bamDir
+cp "$sample"_R1_001_fastqc.html $fastqc_rawDir
+cp "$sample"_R2_001_fastqc.html $fastqc_rawDir
+cp $sample.R1.P_fastqc.html $fastqc_trimmedDir
+cp $sample.R2.P_fastqc.html $fastqc_trimmedDir
+cp $sample.trim.log $trimlogDir
+cp $sample.trim.summary $trimsumDir
+
+if [ -e $sample.R1.U.fastq.gz ];
+then 
+cp $sample.R1.U.fastq.gz $unpairedDir
+fi 
+if [ -e $sample.R2.U.fastq.gz ];
+then
+cp $sample.R2.U.fastq.gz $unpairedDir
+fi
+
+
+
+cd ~/
+
+rm -rf $tempDir
+
