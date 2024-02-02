64206a82507ddd490787246c79ace36cadc41bb6
Courtney Hatton <101742079+clhatton@users.noreply.github.com>
2024-02-02 11:37:56 -0500

    Update run-pipe-PE-STAR-hg38.sh
    changing base directory

--- run-pipe-PE-STAR-hg38.sh
+++ run-pipe-PE-STAR-hg38.sh
@@ -12,7 +12,7 @@ R2="$sample"_R2_001.fastq.gz
 nCores=24
 
 #baseDir=/pi/zhiping.weng-umw/data/hattonc/PRODMAT_Corvera/RNASeq/PRODMAT/STAR/
-baseDir=/zata/zippy/hattonc/PRODMAT/ 
+baseDir=/data/rusers/hattonc/PRODMAT/ 
 fastqDir=/zata/data/zlab/projects/PRODMAT/
 #fastqDir=/zata/zippy/hattonc/test_fastq/
 referenceDir=/zata/zippy/hattonc/Genomes/hg38/RSEM/STAR/
