ad85d200645179239e47c50f543de1e898df3a27
Courtney Hatton <101742079+clhatton@users.noreply.github.com>
2024-02-02 10:56:55 -0500

    Create flagstat.sh

---
+++ flagstat.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+set -e
+
+source /zata/zippy/hattonc/miniconda3/etc/profile.d/conda.sh
+conda activate rnaseq
+
+sample=$1
+
+genome=$sample.STAR.genome.sorted.bam
+transcript=$sample.transcript.sorted.bam
+
+nCores=24
+
+baseDir=/data/rusers/hattonc/PRODMAT/
+bamDir=/data/rusers/hattonc/PRODMAT/BAMs
+genomeDir=/data/rusuers/hattonc/PRODMAT/Mapping_Logs_Genome
+transcriptDir=/data/rusers/hattonc/PRODMAT/Mapping_Logs_Transcript
+
+tempDir=/tmp/hattonc/$sample
+
+rm -rf $tempDir; mkdir -p $tempDir; cd $tempDir
+cp /$bamDir/$genome /$tempDir
+cp /$bamDir/$transcript /$tempDir
+
+samtools flagstat @ $nCores $genome > $sample.genome.maplog.txt
+samtools flagstat @ $nCores $transcript > $sample.transcript.maplog.txt
+
+cp $sample.genome.maplog.txt $genomeDir
+cp $sample.transcript.maplog.txt $transcriptDir
+
+cd $baseDir
+
+rm -rf $tempDir
