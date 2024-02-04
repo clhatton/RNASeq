2a62e0fda01a9b3314dc5b1670e738af1291d4d6
Courtney Hatton <101742079+clhatton@users.noreply.github.com>
2024-02-04 17:40:04 -0500

    Delete execute2.sh

--- execute2.sh
+++
@@ -1,3 +0,0 @@
-for x in $(cat PRODMAT_samples.txt | cut -f1 ); do echo $x; sbatch -N 1 -n 24 --mem=90G -t 240 -p 4hours \
--o /data/rusers/hattonc/logs/flagstat/PRODMAT$x.out -e /data/rusers/hattonc/logs/flagstat/PRODMAT$x.err -J PRODMAT$x \
---wrap="bash flagstat.sh $x"; sleep 1; done
