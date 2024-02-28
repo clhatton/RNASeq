77edd9dfc3bdc55d391f8faa2de014852d27dfcc
Courtney Hatton <101742079+clhatton@users.noreply.github.com>
2024-02-28 16:34:51 -0500

    execute update

--- execute.sh
+++ execute.sh
@@ -1,3 +1,3 @@
 for x in $(cat ./PRODMAT_samples.txt | cut -f1 ); do echo $x; sbatch -N 1 -n 24 --mem=90G -t 240 -p 4hours \
--o ~/logs/hisat2/PRODMAT$x.out -e ~/logs/hisat2/PRODMAT$x.err -J PRODMAT$x \
+-o /zata/zippy/hattonc/logs/hisat2/PRODMAT$x.out -e /zata/zippy/hattonc/logs/hisat2/PRODMAT$x.err -J PRODMAT$x \
 --wrap="bash run-pipe-PE-HISAT2-hg38.sh $x"; sleep 1; done
