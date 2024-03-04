b7e5e9b9726df971d80905c8a1db4041ed1017ae
Courtney Hatton <101742079+clhatton@users.noreply.github.com>
2024-03-04 17:16:51 -0500

    merge file updated more

--- counts_merge.py
+++ counts_merge.py
@@ -1,24 +1,24 @@
 import pandas as pd
 
 file = pd.read_csv('PRODMAT_samples.txt', header=None)
-df_C = pd.read_csv('blank_Genes.txt')
-df_M = pd.read_csv('blank_Genes.txt')
-df_F = pd.read_csv('blank_Genes.txt')
+df_C = pd.read_csv('blank_Genes.txt', header=0)
+df_M = pd.read_csv('blank_Genes.txt', header=0)
+df_F = pd.read_csv('blank_Genes.txt', header=0)
 
 base_path = '/data/rusers/hattonc/PRODMAT/HISAT2/Counts_txt/'
 
 for index, row in file.iterrows():
     if 'C' in row[0]:
         file_path = f'{base_path}{row[0]}_counts.txt'
-        new_df = pd.read_csv(file_path, sep=" ")
+        new_df = pd.read_csv(file_path, sep=" ", header=0)
         df_C = pd.merge(df_C, new_df, on='Gene')
     elif 'M' in row[0]:
         file_path = f'{base_path}{row[0]}_counts.txt'
-        new_df = pd.read_csv(file_path, sep=" ")
+        new_df = pd.read_csv(file_path, sep=" ", header=0)
         df_M = pd.merge(df_M, new_df, on= 'Gene')
     elif 'F' in row[0]:
         file_path = f'{base_path}{row[0]}_counts.txt'
-        new_df = pd.read_csv(file_path, sep=" ")
+        new_df = pd.read_csv(file_path, sep=" ", header=0)
         df_F = pd.merge(df_F, new_df, on= 'Gene')
 
 df_C.to_csv('HISAT2_C.csv', index=False)
